21acd5f7d0ab3716bbbf80bed381df97669bbda0
JGM01 <jacobgermanamccray@gmail.com>
2022-02-04 00:55:13 -0500

    Rebuilt web_server using external threadpool crate

---
+++ src/bin/main.rs
@@ -0,0 +1,43 @@
+extern crate threadpool;
+
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use std::fs;
+use threadpool::ThreadPool;
+
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        
+        pool.execute(|| {
+            handle_connection(stream)
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0;512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, web_page) = 
+        if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK\r\n\r\n", "website/index.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "website/not_found.html")
+        };
+
+    let contents = fs::read_to_string(web_page).unwrap();
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
